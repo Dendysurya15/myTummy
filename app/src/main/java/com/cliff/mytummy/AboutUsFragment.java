7cc7180d0b77c0cba6bc95ffa08b7f07cb806808
surya_dendy <17523078@students.uii.ac.id>
2019-12-04 14:24:03 +0700

    menyelesaikan logika intent buat menampilkan halaman 4 fitur

--- app/src/main/java/com/cliff/mytummy/AboutUsFragment.java
+++ app/src/main/java/com/cliff/mytummy/AboutUsFragment.java
@@ -5,10 +5,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutUsFragment extends Fragment {
 
@@ -16,6 +18,16 @@ public class AboutUsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_about_us_fragment,container,false);
+        final View view = inflater.inflate(R.layout.activity_about_us_fragment,container,false);
+        TextView coba = view.findViewById(R.id.reset_coba);
+        coba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PrefManager prefManager = new PrefManager(getContext());
+                prefManager.setFirstTimeLaunch(true);
+                startActivity(new Intent(getActivity(),SplashScreen.class));
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/cliff/mytummy/HomeFragment.java
+++ app/src/main/java/com/cliff/mytummy/HomeFragment.java
@@ -2,19 +2,81 @@ package com.cliff.mytummy;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 public class HomeFragment extends Fragment {
+    private ArrayList<Fitur> mFiturData;
+    private RecyclerView mRecyclerView;
+    private adapterFitur mAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_home_fragment,container,false);
+
+
+        final View view =  inflater.inflate(R.layout.activity_home_fragment,container,false);
+        mRecyclerView = view.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mFiturData = new ArrayList<>();
+        mAdapter = new adapterFitur(getContext(),mFiturData);
+        mRecyclerView.setAdapter(mAdapter);
+
+        Initialize();
+
+
+
+
+//        CardView cardView1 = view.findViewById(R.id.card_diagnosis);
+//        cardView1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast toast = Toast.makeText(getActivity(),"ini Menu diagnosis penyakit perut",
+//                        Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+        return view;
     }
+
+    public void Initialize(){
+        String[] fiturList = getResources()
+                .getStringArray(R.array.judul_card);
+        String[] fiturInfo = getResources()
+                .getStringArray(R.array.penjelasan_card);
+        TypedArray fiturImageResources = getResources()
+                .obtainTypedArray(R.array.image_card);
+
+        // Clear the existing data (to avoid duplication).
+        mFiturData.clear();
+
+        // Create the ArrayList of Sports objects with the titles and
+        // information about each sport
+        for (int i = 0; i < fiturList.length; i++) {
+            mFiturData.add(new Fitur(fiturList[i], fiturInfo[i],
+                    fiturImageResources.getResourceId(i, 0)));
+        }
+
+        // Recycle the typed array.
+        fiturImageResources.recycle();
+
+        // Notify the adapter of the change.
+        mAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * onClick method for th FAB that resets the data.
+     *
+     * @param view The button view that was clicked.
+     */
+
 }
